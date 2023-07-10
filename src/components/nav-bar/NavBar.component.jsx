7d79e1c1b641f82e0336c3003167bcd2b7f053d3
joeebw <joeegaarciia@gmail.com>
2023-07-10 06:18:44 -0600

    setting navbar

--- src/components/nav-bar/NavBar.component.jsx
+++ src/components/nav-bar/NavBar.component.jsx
@@ -1,41 +1,57 @@
-import { Link, Outlet } from 'react-router-dom'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import Footer from '../../components/footer/Footer.component'
-import logo from '../../assets/Logo.png'
-import { Typography } from '@mui/material';
+import { Link, Outlet } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Footer from "../../components/footer/Footer.component";
+import logo from "../../assets/Logo.png";
+import { Typography } from "@mui/material";
 
 const buttonStyle = {
-  fontFamily: 'Poppins, sans-serif',
-  color: '#ffffff'
-}
+  fontFamily: "Poppins, sans-serif",
+  color: "#ffffff",
+};
 
 export default function NavBar() {
-  
-
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="fixed">
-          <Toolbar sx={{display: 'flex', justifyContent:'space-between'}}>
-            <Link to={'/'} style={{textDecoration: 'none'}}>
-              <img src={logo} alt=""  style={{width: '80px',paddingLeft: '50px'}}/>
-            </Link>
-            <Box sx={{display: 'flex', gap: '15px'}}>
-              <Link to={'/'}>
-                <Button variant='text' sx={buttonStyle}>Inicio</Button>
+          <Toolbar
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "90%",
+              margin: "0 auto",
+            }}
+          >
+            <Box style={{ width: "80px", height: "80px", overflow: "hidden" }}>
+              <Link to={"/"} style={{ textDecoration: "none" }}>
+                <img
+                  src={logo}
+                  alt=""
+                  style={{ width: "100%", height: "100%" }}
+                />
               </Link>
-              <Link to={'/bio'}>
-                <Button variant='text' sx={buttonStyle}>Sobre Mi</Button>
+            </Box>
+            <Box sx={{ display: "flex", gap: "1.7rem" }}>
+              <Link to={"/"}>
+                <Button variant="text" sx={buttonStyle}>
+                  Inicio
+                </Button>
+              </Link>
+              <Link to={"/bio"}>
+                <Button variant="text" sx={buttonStyle}>
+                  Sobre Mi
+                </Button>
               </Link>
             </Box>
           </Toolbar>
         </AppBar>
       </Box>
-      <Outlet/>
-      <Footer/>
+      <Outlet />
+      <Footer />
     </>
   );
 }
--- src/routes/bio/Bio.component.jsx
+++ src/routes/bio/Bio.component.jsx
@@ -1,53 +1,54 @@
-import { useContext, useEffect, useState } from "react"
-import {ScreenContext} from '../../context/screen.context'
-import { Container, Typography } from "@mui/material"
-import { ReactMarkdown } from "react-markdown/lib/react-markdown"
-import Spinner from "../../components/spinner/spinner.component"
-import profileImg from '../../assets/profile-blog.png'
-import './bio.styles.scss'
+import { useContext, useEffect, useState } from "react";
+import { ScreenContext } from "../../context/screen.context";
+import { Container, Typography } from "@mui/material";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import Spinner from "../../components/spinner/spinner.component";
+import profileImg from "../../assets/profile-blog.png";
+import "./bio.styles.scss";
 
 const bioContainerSx = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  marginTop: '140px'
-}
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  marginTop: "140px",
+};
 
 const Bio = () => {
-  const [about, setAbout] = useState('');
+  const [about, setAbout] = useState("");
   const [isReady, setIsReady] = useState(false);
-  const {isSmallScreen} = useContext(ScreenContext);
+  const { isSmallScreen } = useContext(ScreenContext);
 
   useEffect(() => {
-    import('../../posts/about-me.md').then(resp => {
+    import("../../posts/about-me.md").then((resp) => {
       fetch(resp.default)
-      .then(response => response.text())
-      .then(text => {
-        setAbout(text);
-        setTimeout(() => {
-          setIsReady(true);
-        }, 500)
-      })
-    })
-  }, [])
-
+        .then((response) => response.text())
+        .then((text) => {
+          setAbout(text);
+          setTimeout(() => {
+            setIsReady(true);
+          }, 500);
+        });
+    });
+  }, []);
 
   return (
     <>
-      <Spinner isReady={isReady}/> 
-      <Container maxWidth='md'  sx={bioContainerSx}>
-        <Typography variant={isSmallScreen ? "h4" : "h3" } component={'h3'} textAlign={'center'}>
+      <Spinner isReady={isReady} />
+      <Container maxWidth="md" sx={bioContainerSx}>
+        <Typography
+          variant={isSmallScreen ? "h4" : "h3"}
+          component={"h3"}
+          textAlign={"center"}
+        >
           Psicóloga Sheridan
         </Typography>
         <div className="profile-img">
           <img src={profileImg} alt="" />
         </div>
-        <ReactMarkdown className="about-profile">
-          {about}
-        </ReactMarkdown>
+        <ReactMarkdown className="about-profile">{about}</ReactMarkdown>
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default Bio
+export default Bio;
