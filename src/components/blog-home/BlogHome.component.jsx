90d8e1f82074475fb234e8b6516f506d5039f6bc
joeebw <joeegaarciia@gmail.com>
2023-07-14 15:33:26 -0600

    changing header image

--- src/components/blog-home/BlogHome.component.jsx
+++ src/components/blog-home/BlogHome.component.jsx
@@ -1,22 +1,11 @@
 import Header from "../header/Header.component";
 import AboutMe from "../about-me/AboutMe.component";
 import Articles from "../articles/Articles.component";
-import { useContext, useEffect, useState } from "react";
-import Spinner from "../spinner/spinner.component";
 
 const BlogHome = () => {
-  const [isReady, setIsReady] = useState(false);
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setIsReady(true);
-  //   }, 500);
-  // }, []);
-
   return (
     <>
-      <Spinner isReady={isReady} />
-      <Header setIsReady={setIsReady} />
+      <Header />
       <AboutMe />
       <Articles />
     </>
--- src/components/header/Header.component.jsx
+++ src/components/header/Header.component.jsx
@@ -1,18 +1,7 @@
 import "./header.style.scss";
 
-function Header({ setIsReady }) {
-  const handleOnLoad = () => {
-    setIsReady(true);
-  };
-
-  return (
-    <div className="header">
-      <img
-        src="https://images.unsplash.com/photo-1620503292890-c597f62cce8d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-        onLoad={handleOnLoad}
-      />
-    </div>
-  );
+function Header() {
+  return <div className="header"></div>;
 }
 
 export default Header;
